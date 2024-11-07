79931b73d20e5712b09f17c5d5786c7dad75fff3
JuanJavier <juanjcbreton@gmail.com>
2024-11-07 13:06:35 -0600

    7-11-24 Ya se pueden escribir archivos

--- ManipulacionArchivos.java
+++ ManipulacionArchivos.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 
 public class ManipulacionArchivos {
 
@@ -87,21 +89,89 @@ public class ManipulacionArchivos {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
-        String fileName;
-        String[] languages;
-
-        System.out.println("Lectura de archivos de texto");
-        System.out.println("Escribe el nombre del archivo: ");
-        fileName = bufer.readLine();
-        //leerArchivo(fileName);
-
-        languages = fileToStringArray(fileName);
+    public static void printStringArray(String[] array){
         System.out.println("Contenido del arreglo");
-        for ( String element : languages ) {
+        for ( String element : array ) {
             System.out.println(element);
         }
+    }
+
+    public static void writeFile(String fileName) {
+        // Un apuntador a un espacio físico en el disco duro
+        FileWriter file;
+        // Llave de acceso para el archivo
+        PrintWriter writer;
+        // Configurar buffer p leer del teclado
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        String entrada;
+        char respuesta;
+
+        try {
+            // Apuntando a un espacio físico del disco duro
+            file = new FileWriter("c:\\archivos\\" + fileName + ".txt");
+            // Crear y abrir archivo en modo escritura
+            writer = new PrintWriter(file);
+            do {
+                System.out.println("Escribe datos para guardar al archivo: ");
+                entrada = bufer.readLine();
+                // Guardar la entrada del archivo
+                writer.println(entrada);
+                System.out.println("Escriba x para salir, este campo no puede estar vacio");
+                entrada = bufer.readLine();
+                respuesta = entrada.charAt(0);
+            } while ( respuesta != 'x' );
+            file.close();
+        } catch (Exception e) {
+            System.out.println("################ Error al escribir el archivo ################");
+            System.out.println("################ Error:                       ################");
+            System.out.println(e.toString());
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        String fileName, entrada;
+        String[] languages;
+        int opcion;
+
+        // Menú de opciones
+        do {
+            System.out.println("1 - Leer archivo y mostrar en pantalla");
+            System.out.println("2 - Llamar arreglo con contenido de un archivo");
+            System.out.println("3 - Crear archivo desde teclado");
+            System.out.println("4 - Nada");
+            System.out.println("5 - Salir");
+            entrada = bufer.readLine();
+            opcion = Integer.parseInt(entrada);
+            switch (opcion) {
+                case 1: {
+                    System.out.println("Escribe el nombre del archivo: ");
+                    fileName = bufer.readLine();
+                    leerArchivo(fileName);
+                }
+                break;
+                case 2: {
+                    System.out.println("Escribe el nombre del archivo: ");
+                    fileName = bufer.readLine();
+                    languages = fileToStringArray(fileName);
+                    printStringArray(languages);
+                }
+                break;
+                case 3: {
+                    System.out.println("Escribe el nombre del archivo: ");
+                    fileName = bufer.readLine();
+                    writeFile(fileName);
+                }
+                break;
+                case 5: {
+                    System.out.println("Saliendo del sistema");
+                    System.exit(0);
+                }
+                break;
+                default:
+                    break;
+            }
+        } while (opcion != 5);
 
     }
 
